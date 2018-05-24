e483e9a62deeb659ab8fabf17ba49bf8132456e0
iamsss <saurav9760@gmail.com>
2018-05-25 00:04:34 +0530

    Setting basics Server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,23 @@
+// BuitIn Variable
 const path = require('path');
 
+// Module VAriable Initilization
+const express = require('express');
+
+
+// Loading Addition Site File
+
+
+// declaring Global variable
+var port = 3000;
+var app = express();
 const publicPath = path.join(__dirname, '../public');
 
-console.log(__dirname + '/../public');
 
-console.log(publicPath);
+
+// Middleware
+app.use(express.static(publicPath));
+
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));

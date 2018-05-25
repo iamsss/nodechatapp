c73513efe2aff48a171d59e089c9adc7bcefa76c
iamsss <saurav9760@gmail.com>
2018-05-25 16:15:43 +0530

    Adding little FrontEnd to send chat

--- public/js/index.js
+++ public/js/index.js
@@ -9,17 +9,31 @@ socket.on('disconnect',function() {
 });
 
 socket.on('newMessage', function(data){
-console.log('New Message',data);
+    var li = $('<li></li>');
+    li.text(`${data.from} : ${data.text}`);
+    $('#messages').append(li);
 }); // Listening newMessage emit event of server
 
 
 
-socket.emit('createMessage',{
-        from: 'saurav',
-        text: 'Hi There'
+// socket.emit('createMessage',{
+//         from: 'saurav',
+//         text: 'Hi There'
+//     }, function(data) {
+//         console.log('Got it',data);
+//     }); // Methods to Send Data from client to serve
+
+
+$('#message-form').on('submit', function(e) {
+    e.preventDefault();
+    var text = $('[name=message]').val();
+    var from = 'saurav';
+
+    socket.emit('createMessage',{
+        from,
+        text
     }, function(data) {
         console.log('Got it',data);
     }); // Methods to Send Data from client to serve
 
-
-
+})
